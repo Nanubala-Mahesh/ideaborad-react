fb40e3ad7f063bb6456b18432d1dfa7c2c455f35
maheshbablu17 <mahesh.bablu17@gmail.com>
2018-05-10 12:49:27 +0530

    add new action and notification

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -11,8 +11,13 @@ class IdeasContainer extends Component {
     super(props)
     this.state = {
       ideas: [],
-      editingIdeaId: null
+      editingIdeaId: null,
+      notification: ''
     }
+  }
+
+  resetNotification = () => {
+    this.setState({notification: ''})
   }
 
   componentDidMount() {
@@ -51,6 +56,16 @@ class IdeasContainer extends Component {
     .catch(error => console.log(error))
   }
 
+  updateIdea = (idea) => {
+    const ideaIndex = this.state.ideas.findIndex(x => x.id === idea.id)
+    const ideas = update(this.state.ideas, {
+      [ideaIndex]: { $set: idea }
+    })
+    this.setState({
+      ideas: ideas,
+      notification: "All changes saved"
+    })
+  }
 
   render(){
     return(
@@ -62,11 +77,15 @@ class IdeasContainer extends Component {
           New Idea
         </button>
 
+        <span className="notification">
+          {this.state.notification}
+        </span>
+
         {this.state.ideas.map((idea) =>{
           if (this.state.editingIdeaId === idea.id) {
-            return(<IdeaForm idea={idea} key={idea.id} />)
+            return(<IdeaForm idea={idea} key={idea.id} updateIdea={this.updateIdea} resetNotification={this.resetNotification} />)
           } else {
-            return(<Idea idea={idea} key={idea.id} />)
+            return(<Idea idea={idea} key={idea.id} updateIdea={this.updateIdea}/>)
           }
         })}
       </div>
